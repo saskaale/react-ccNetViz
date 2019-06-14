7363071789d6094f0f3e1cfeaacf97706bb74b04
Ales Saska <mail@alessaska.cz>
2019-06-14 17:05:26 -0500

    added origin component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import CCNetVizComponent from './ccNetVizComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const options = {
+  styles: {
+    node: { texture: "images/node.png", label: { hideSize: 16 } },
+    edge: { arrow: { texture: "images/arrow.png" } }
+}};
+
+var nodes = [
+  { label: "Hello", x: 0, y: 1 },
+  { label: "World", x: 1, y: 1  },
+  { label: "!", x: 0.5, y: 0  },
+  { label: "I am in center", x: 0.5, y: 0.5 },
+  { label: "Right bottom corner", x: 1, y: 0 },
+];
+var edges = [
+  { source: nodes[0], target: nodes[1] },
+  { source: nodes[1], target: nodes[0] },
+  { source: nodes[0], target: nodes[0] },
+  { source: nodes[1], target: nodes[2] },
+  { source: nodes[2], target: nodes[4] }
+];
+
+
+const App = () => {
+  const [w, setw] = useState(640);
+  const [h, seth] = useState(480);
+
+  useEffect(() => {
+    const int = setInterval(() => {
+      setw(w < 1000 ? w+10 : 640);
+      seth(h < 800 ? h+10 : 480);
+    },2*1000);
+
+    return () => clearInterval(int);
+  });
+
+  console.log(`SIZE [${w}x${h}]`);
+
+  return (<div>
+    <h1>ccNetViz example</h1>
+      <div>
+        <CCNetVizComponent
+          width={w}
+          height={h}
+          options={options}
+          nodes={nodes}
+          edges={edges}
+          />
+      </div>
+    </div>)
 }
 
 export default App;
+
